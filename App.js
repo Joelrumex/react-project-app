307b7f76d4eca4c4af1224c155df2a737f523112
Joel <60098544+Joelrumex@users.noreply.github.com>
2024-10-07 12:54:15 +0200

    background

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, ImageBackground } from 'react-native';
 import { Image } from 'expo-image';
 
 export default function App() {
@@ -12,32 +12,41 @@ export default function App() {
   };
 
   return (
-    <View style={styles.container}>
-      {/* Puntuació a la part superior dreta */}
-      <View style={styles.counterContainer}>
-        <Text style={styles.counterText}>{"Eggs:"}</Text>
-        <Text style={styles.counterText}>{count}</Text>
+    <ImageBackground 
+      source={require('./assets/background.jpg')} // Ruta de la imatge de fons
+      style={styles.background}
+      resizeMode="cover" // Aquesta opció farà que la imatge de fons ocupi tota la pantalla
+    >
+      <View style={styles.container}>
+        {/* Puntuació a la part superior dreta */}
+        <View style={styles.counterContainer}>
+          <Text style={styles.counterText}>{count}</Text>
+        </View>
+        
+        <Text style={styles.instructions}>Click the chicken to increase the count!</Text>
+        
+        {/* GIF substituint el botó */}
+        <TouchableOpacity onPress={handleClick}>
+          <Image
+            style={styles.chickenGif}
+            source={require('./assets/CluckingChickenIdleSide.gif')} // Ruta del GIF
+          />
+        </TouchableOpacity>
+        
+        <StatusBar style="auto" />
       </View>
-      
-      <Text style={styles.instructions}>Click the chicken to increase the count!</Text>
-      
-      {/* GIF substituint el botó */}
-      <TouchableOpacity onPress={handleClick}>
-        <Image
-          style={styles.chickenGif}
-          source={require('./assets/CluckingChickenIdleSide.gif')} // Ruta del GIF
-        />
-      </TouchableOpacity>
-      
-      <StatusBar style="auto" />
-    </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
@@ -53,9 +62,10 @@ const styles = StyleSheet.create({
   instructions: {
     marginBottom: 20,
     fontSize: 16,
+    color: '#fff',
   },
   chickenGif: {
-    width: 20,
-    height: 20,
+    width: 100,
+    height: 100,
   },
 });
